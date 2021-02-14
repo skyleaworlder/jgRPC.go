c3cb361372d977497442bc0123a6ffec87bdf7ac
skyleaworlder <870033938@qq.com>
2021-02-14 18:01:32 +0800

    M: change HID from []byte to uint64, since []byte is an unhashable type

--- LdBlsServer/ldbls/ConsistHash/conhash.go
+++ LdBlsServer/ldbls/ConsistHash/conhash.go
@@ -1,6 +1,10 @@
 package conhash
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/skyleaworlder/jgRPC.go/LdBlsServer/ldbls"
 	sm "github.com/umpc/go-sortedmap"
 )
@@ -12,6 +16,67 @@ type conhash struct {
 // newConHash is a function to create an obj
 func newConHash() *conhash {
 	res := new(conhash)
-	res.HT = sm.New(ldbls.M, HIDComp)
+	res.HT = sm.New(ldbls.M, NodeComp)
 	return res
+}
+
+func (c *conhash) GetNode(HID uint64) (*ldbls.Node, error) {
+	iter, err := c.HT.IterCh()
+	if err != nil {
+		msg := "Fatal Error: conhash.GetNode cannot generate an iter\n"
+		fmt.Fprint(os.Stderr, err.Error()+msg)
+		return &ldbls.Node{}, errors.New(err.Error() + msg)
+	}
+	defer iter.Close()
+	for rec := range iter.Records() {
+		fmt.Println("rec:", rec)
+	}
+	return &ldbls.Node{}, nil
+}
+
+func (c *conhash) PostNode(n *ldbls.Node) (*ldbls.Node, error) {
+	c.HT.Insert(n.HID, n)
+	return n, nil
+}
+
+func (c *conhash) DeleteNode(HID uint64) (*ldbls.Node, error) {
+	node, ok := c.HT.Get(HID)
+	if !ok {
+		msg := "Fatal Error: conhash.DeleteNode cannot get node from HT\n"
+		fmt.Fprint(os.Stderr, msg)
+		return node.(*ldbls.Node), errors.New(msg)
+	}
+	// try to delete node using HID
+	c.HT.Delete(HID)
+	return node.(*ldbls.Node), nil
+}
+
+// PutNode method's 3 phases:
+// 1. check whether node given is in HT.
+// 2. delete the node in HT that own the same key as given node.
+// 3. insert a new node which is with the same key as the deleted node,
+//    but with different properties, like addr, port, etc.
+func (c *conhash) PutNode(n *ldbls.Node) (*ldbls.Node, error) {
+	// since sm.Replace don't have return val, I don't use c.HT.Replace(n.HID, n)
+	if !c.HT.Has(n.HID) {
+		msg := "Fatal Error: conhash.PutNode cannot use sm.Has function to find node from HT\n"
+		fmt.Fprint(os.Stderr, msg)
+		return &ldbls.Node{}, errors.New(msg)
+	}
+
+	_, err := c.DeleteNode(n.HID)
+	if err != nil {
+		msg := "Fatal Error: conhash.PutNode cannot use DeleteNode to delete node from HT\n"
+		fmt.Fprint(os.Stderr, err.Error()+msg)
+		return &ldbls.Node{}, errors.New(err.Error() + msg)
+	}
+
+	node, err := c.PostNode(n)
+	if err != nil {
+		msg := "Fatal Error: conhash.PutNode cannot use PostNode to insert node into HT\n"
+		fmt.Fprint(os.Stderr, err.Error()+msg)
+		return node, errors.New(err.Error() + msg)
+	}
+
+	return node, nil
 }
--- LdBlsServer/ldbls/ConsistHash/utils_test.go
+++ LdBlsServer/ldbls/ConsistHash/utils_test.go
@@ -4,21 +4,27 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"io"
+	"net"
 	"testing"
+
+	"github.com/skyleaworlder/jgRPC.go/LdBlsServer/ldbls"
 )
 
 func Test_HIDComp(t *testing.T) {
 	fmt.Println("1. HIDComp:")
 	fmt.Println("1.1 simple test:")
-	fmt.Println(HIDComp([]byte{0x01}, []byte{0x02}))
-	fmt.Println(HIDComp([]byte{0x02}, []byte{0x02}))
-	fmt.Println(HIDComp([]byte{0x03}, []byte{0x02}))
-
-	fmt.Println("1.2 crypto/sha1 test:")
-	ha, hb, hc := sha1.New(), sha1.New(), sha1.New()
 	addra := "192.168.1.1:5000"
 	addrb := "127.0.0.1:3000"
 	addrc := "192.168.1.1:5001"
+	anode := ldbls.Node{HID: 162, IP: net.ParseIP(addra), PORT: 5000}
+	bnode := ldbls.Node{HID: 163, IP: net.ParseIP(addrb), PORT: 3000}
+	cnode := ldbls.Node{HID: 164, IP: net.ParseIP(addrc), PORT: 5001}
+	fmt.Println(NodeComp(&anode, &bnode))
+	fmt.Println(NodeComp(&bnode, &cnode))
+	fmt.Println(NodeComp(&cnode, &anode))
+
+	fmt.Println("1.2 crypto/sha1 test:")
+	ha, hb, hc := sha1.New(), sha1.New(), sha1.New()
 	io.WriteString(ha, addra)
 	io.WriteString(hb, addrb)
 	io.WriteString(hc, addrc)
@@ -26,7 +32,4 @@ func Test_HIDComp(t *testing.T) {
 	fmt.Printf("ha's res: %x\n", ares)
 	fmt.Printf("hb's res: %x\n", bres)
 	fmt.Printf("hc's res: %x\n", cres)
-	fmt.Println("ha's res and hb's res:", HIDComp(ares, bres))
-	fmt.Println("hb's res and hc's res:", HIDComp(bres, cres))
-	fmt.Println("hc's res and ha's res:", HIDComp(cres, ares))
 }
