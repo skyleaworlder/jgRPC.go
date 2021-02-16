ba9bbf061f08e71c3b96dd62e1928e9b69cc1961
skyleaworlder <870033938@qq.com>
2021-02-16 19:03:21 +0800

    M: init server

--- nodeserver/nodeserver.go
+++ nodeserver/nodeserver.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	jgut "github.com/skyleaworlder/jgRPC.go/jgrpcUtils"
+	jgrpcs "github.com/skyleaworlder/jgRPC.go/nodeserver/jgrpc"
+)
 
 // NodeServer should implement several functions:
 // 1. service register
@@ -13,6 +21,57 @@ import "fmt"
 // 3. communicate with load balance/cache Server
 // 	receive CALL(0) request, then send execute results.
 
+const (
+	cfgMaxNum = 16
+)
+
+var (
+	// Config is a map
+	// e.g. "Local_IP" => "127.0.0.1"
+	Config = make(map[string]string, cfgMaxNum)
+
+	// Calcu is the obj of calculator
+	Calcu = new(jgrpcs.Calculator)
+)
+
 func main() {
-	fmt.Println("Hello World!")
+	jgut.Readcfg(Config, "NodeServer.cfg")
+	Calcu.Init()
+	Calcu.Config = Config
+
+	listener, err := net.Listen("tcp4", Config["Listen_Port"])
+	if err != nil {
+		msg := "Warning: Node Server listener initialization failed\n"
+		fmt.Fprint(os.Stderr, msg)
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			msg := "Warning: Node Server listener failed\n"
+			fmt.Fprint(os.Stderr, msg)
+			continue
+		}
+
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	// set 5s ddl
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.Close()
+
+	buf := make([]byte, 256)
+	_, err := conn.Read(buf)
+	if err != nil {
+		msg := "Warning: Node Server handleRequest failed\n"
+		fmt.Fprint(os.Stderr, msg)
+		return
+	}
+
+	resp := make([]byte, 256)
+	resp = getResponse(buf)
+	conn.Write(resp)
 }
